b3bb40b584f31cb76fc7edb29f4ae534df93a996
Vitaliy <jarov_vi@mail.ru>
2026-03-04 16:19:27 +0500

    1

--- src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
+++ src/main/java/ru/yandex/practicum/catsgram/CatsgramApplication.java
@@ -2,14 +2,23 @@ package ru.yandex.practicum.catsgram;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Scanner;
 
 public class CatsgramApplication {
+
+
+    private static final Logger log = LoggerFactory.getLogger(CatsgramApplication.class);
+
     public static void main(String[] args) {
         final Gson gson = new Gson();
         final Scanner scanner = new Scanner(System.in);
+
+        log.info("Hello");
+
         System.out.print("Введите JSON => ");
         final String input = scanner.nextLine();
         try {
