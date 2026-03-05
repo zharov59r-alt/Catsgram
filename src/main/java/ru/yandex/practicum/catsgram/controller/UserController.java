eacfa60af111fdf9ac58b908deceff15efc7d88a
Vitaliy <jarov_vi@mail.ru>
2026-03-05 13:21:33 +0500

    1

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,91 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        // проверяем выполнение необходимых условий
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Имейл должен быть указан");
+        }
+        if (users.values()
+                .stream()
+                .map(User::getEmail)
+                .anyMatch(user.getEmail()::equals)) {
+            throw new DuplicatedDataException("Этот имейл уже используется");
+        }
+        // формируем дополнительные данные
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        // сохраняем новую публикацию в памяти приложения
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        // проверяем необходимые условия
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+
+            if (users.values()
+                    .stream()
+                    .filter(u -> !u.equals(oldUser))
+                    .map(User::getEmail)
+                    .anyMatch(newUser.getEmail()::equals)) {
+                throw new DuplicatedDataException("Этот имейл уже используется");
+            }
+
+            if (newUser.getEmail() != null && !newUser.getEmail().isBlank()) {
+                oldUser.setEmail(newUser.getEmail());
+            }
+
+            if (newUser.getUsername() != null && !newUser.getUsername().isBlank()) {
+                oldUser.setUsername(newUser.getUsername());
+            }
+
+            if (newUser.getPassword() != null && !newUser.getPassword().isBlank()) {
+                oldUser.setPassword(newUser.getPassword());
+            }
+
+            return oldUser;
+        }
+        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
+    }
+
+    // вспомогательный метод для генерации идентификатора нового поста
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+
+
+}
